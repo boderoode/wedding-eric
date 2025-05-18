7ba09128e627709907492c87617dcd037b634c0f
boderoode <79194913+boderoode@users.noreply.github.com>
2025-05-18 15:59:27 +0200

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,16 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources 'rsvps' do
+  resources "rsvps" do
   end
 
-  resources 'gifts' do
+  resources "gifts" do
+  end
+
+  resources "faq" do
+  end
+
+  resources "hotels" do
   end
 
   # Defines the root path route ("/")
